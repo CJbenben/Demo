1246d01e9038b257afd3faa6c131f490ac8b5957
ChenJie <jie.chen1@atzuche.cn>
2018-01-26 16:13:42 +0800

    AtzucheCycleScrollview

--- Demo/CollectionDemo/ATCollectionViewDemoVC.m
+++ Demo/CollectionDemo/ATCollectionViewDemoVC.m
@@ -7,19 +7,61 @@
 //
 
 #import "ATCollectionViewDemoVC.h"
+//#import "CJCycleScrollView.h"
+#import "AtzucheCycleScrollView.h"
 
 @interface ATCollectionViewDemoVC ()
+
+//@property (strong, nonatomic) CJCycleScrollView *scrollView;
+@property (nonatomic, strong) AtzucheCycleScrollView *atScrollView;
+
+@property (nonatomic, strong) NSMutableArray *imageAry;
 
 @end
 
 @implementation ATCollectionViewDemoVC
 
+- (NSMutableArray *)imageAry {
+    if (_imageAry == nil) {
+        _imageAry = [@[@"http://down.tutu001.com/d/file/20110312/ae15ebaebfa15428826432e50e_560.jpg",
+                       @"http://down.tutu001.com/d/file/20120315/29e47a7c992f6f1d978bfc6d8d_560.jpg",
+                       @"http://pic23.nipic.com/20120919/10785657_204032524191_2.jpg",
+                       @"http://pic39.nipic.com/20140310/18084329_102707924000_2.jpg"] mutableCopy];
+    }
+    return _imageAry;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
     self.title = @"ATCollectionViewDemoVC";
     
+    //[self addCJScrollViewDemo];
+    
+    [self addAtzucheScrollViewDemo];
 }
+
+- (void)addAtzucheScrollViewDemo {
+    CGRect scrollviewF = CGRectMake(0, 64 + 20, SCREEN_WIDTH, 220);
+    CGRect frame = CGRectMake(20, 0, SCREEN_WIDTH - 40, 220);
+    
+    self.atScrollView = [AtzucheCycleScrollView atzucheCycleScrollViewFrame:scrollviewF imageViewFrame:frame radius:10.0 imagePaths:self.imageAry animationDuration:2.0];
+    [self.view addSubview:self.atScrollView];
+    
+    self.atScrollView.TapActionBlock = ^(NSInteger pageIndex) {
+        NSLog(@"index = %ld", pageIndex);
+    };
+    
+}
+
+//- (void)addCJScrollViewDemo {
+//    CGRect scrollviewF = CGRectMake(0, 64 + 20, SCREEN_WIDTH, 220);
+//    CGRect frame = CGRectMake(20, 64 + 20, SCREEN_WIDTH - 40, 220);
+//    self.scrollView = [CJCycleScrollView cjCycleScrollViewFrame:scrollviewF imageViewFrame:frame radius:5.0 imagePaths:self.imageAry animationDuration:2.0];
+//    [self.view addSubview:self.scrollView];
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
