e8574986a03cfa3bb3039029574966e0ffc5eea8
chenxiaojie <chenjie003@qq.com>
2019-05-09 16:42:57 +0800

    soudnoti

---
+++ SoundNoti/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  SoundNoti
+//
+//  Created by chenxiaojie on 2019/4/29.
+//  Copyright © 2019年 ChenJie. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
